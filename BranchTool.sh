87cba579fa9cf1322025dd8711ef6aace4116b7c
panidummy <panikumar1001@gmail.com>
2020-12-31 22:55:27 +0000

    enhanced

--- BranchTool.sh
+++ BranchTool.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 demo()
 {
+if [ "$1" = "CREATE" ]; then 
 echo "clone existing repository"
-git clone https://github.com/panidummy/UKB-LIVE-PMF.git $1
-cd $1
+git clone https://github.com/panidummy/UKB-LIVE-PMF.git $2
+cd $2
 echo "${PWD}"
 git status
 cd ..
 echo "${PWD}"
+else
+echo "pull existing repo."
+cd $2
+git pull
+fi
 }
 
-demo $1
+demo $1 $2
 
 
 
